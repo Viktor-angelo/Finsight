3f22c4ec37e4b1ad9a4df0b47dcda00597c9573c
viktor Angelo <angeloviktor885@gmail.com>
2026-06-05 17:32:20 -0300

    new-server-update

--- backend/server.js
+++ backend/server.js
@@ -13,20 +13,10 @@ const allowedOrigins = [
 
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: allowedOrigins,
     credentials: true,
   }),
 );
-
-app.options("*", cors());
 
 app.use(express.json());
 
