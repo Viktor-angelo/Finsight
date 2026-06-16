42183e47ab0e991a8d7e701d007b9010e76850d0
viktor Angelo <angeloviktor885@gmail.com>
2026-06-16 19:00:01 -0300

    correct layout update2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter } from "react-router-dom";
+import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
