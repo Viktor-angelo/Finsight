2f728ce93d4c3b143b6300acfd6644a1c2181416
viktor Angelo <angeloviktor885@gmail.com>
2026-06-16 19:07:58 -0300

    layout definitive

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./context/AuthProvider";
 import { BrowserRouter } from "react-router-dom";
-import "./global.css";
+import "./style/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
